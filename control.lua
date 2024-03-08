ee67a9c0f226b29733dd5b75845557d55fa5452c
TheTaurunti <153017348+TheTaurunti@users.noreply.github.com>
2024-03-08 10:17:08 -0800

    went kind of crazy with changes. overall improvements, belt setting copy is neat, not yet sure if I want the copy settings from container, as that's well-covered in vanilla already

--- control.lua
+++ control.lua
@@ -1,6 +1,6 @@
--- ============================================
--- Set up variables for in-script data transfer
--- ============================================
+-- =============================================
+-- Set up variables for tracking copied settings
+-- =============================================
 
 local _item_name = nil
 local _item_stack_size = nil
@@ -9,6 +9,19 @@ local function clear_copied_info()
   _item_name = nil
   _item_stack_size = nil
 end
+
+local function set_item_name_and_stack(item_name)
+  if (not item_name) then return false end
+  if (game.item_prototypes[item_name])
+  then
+    _item_name = item_name
+    _item_stack_size = game.item_prototypes[_item_name].stack_size
+    return true
+  else
+    return false
+  end
+end
+
 
 -- ================================
 -- Hardcoded Numbers Initialization
@@ -24,6 +37,12 @@ local _MAGIC_NUMBER_STACK_LIMIT_PROGRESSION = {
   [40] = 1 -- allow for looping back around, so fixing a mistake is easy
 }
 
+local _LOADER_ENTITY_TYPES = {
+  ["loader"] = true,
+  ["loader-1x1"] = true,
+  ["loader-1x2"] = true
+}
+
 -- =================
 -- Utility Functions
 -- =================
@@ -36,121 +55,72 @@ local function standardize_vector(vector)
   return ((vector.x and vector.y) and vector) or { x = vector[1], y = vector[2] }
 end
 
-local function get_real_pickup_dropoff_positions(inserter_entity)
-  local direction = inserter_entity.direction
-  local inserter_position = standardize_vector(inserter_entity.position)
-  local pickup = standardize_vector(inserter_entity.prototype.inserter_pickup_position)
-  local dropoff = standardize_vector(inserter_entity.prototype.inserter_drop_position)
-
-  -- Inserter pickup/drop vectors need a translation
-  -- ... based on the entity's rotation.
-  local ret_pickup = nil
-  local ret_dropoff = nil
+local function rotate_vector_from_north(vector, direction)
   if (direction == defines.direction.north)
   then
-    ret_pickup = { x = pickup.x, y = pickup.y }
-    ret_dropoff = { x = dropoff.x, y = dropoff.y }
-    goto end_inserter_rotation_vector_adjustment
+    return { x = vector.x, y = vector.y }
   end
   if (direction == defines.direction.south)
   then
-    ret_pickup = { x = pickup.x, y = 0 - pickup.y }
-    ret_dropoff = { x = dropoff.x, y = 0 - dropoff.y }
-    goto end_inserter_rotation_vector_adjustment
+    return { x = vector.x, y = 0 - vector.y }
   end
   if (direction == defines.direction.east)
   then
-    ret_pickup = { x = 0 - pickup.y, y = pickup.x }
-    ret_dropoff = { x = 0 - dropoff.y, y = dropoff.x }
-    goto end_inserter_rotation_vector_adjustment
+    return { x = 0 - vector.y, y = vector.x }
   end
   if (direction == defines.direction.west)
   then
-    ret_pickup = { x = pickup.y, y = pickup.x }
-    ret_dropoff = { x = dropoff.y, y = dropoff.x }
-    goto end_inserter_rotation_vector_adjustment
-  end
-  ::end_inserter_rotation_vector_adjustment::
-
-  -- This only happens if rotation is diagonal.
-  -- >> I return nil because ??? how to rotate for diagonal direction
-  if (not ret_pickup or not ret_dropoff) then return nil end
-
-  local ret = {
-    pickup = {
-      inserter_position.x + ret_pickup.x,
-      inserter_position.y + ret_pickup.y
-    },
-    drop = {
-      inserter_position.x + ret_dropoff.x,
-      inserter_position.y + ret_dropoff.y
-    }
-  }
-
-  return ret
-end
-
-local function get_container(surface, position)
-  local at_pickup = surface.find_entities_filtered {
-    position = position,
-    type = { "container", "logistic-container" }
-  }
-  return (#at_pickup > 0 and at_pickup[1]) or nil
-end
-
-local function get_next_stack_amount(current_stacks)
-  local immediate_lookup = _MAGIC_NUMBER_STACK_LIMIT_PROGRESSION[current_stacks]
-  if (immediate_lookup) then return immediate_lookup end
-
-  local closest = { index = 1, diff = 1000 }
-  for k, v in pairs(_MAGIC_NUMBER_STACK_LIMIT_PROGRESSION) do
-    local difference = math.abs(current_stacks - v)
-    if (difference <= closest.diff)
-    then
-      closest.index = k
-      closest.diff = difference
-    end
+    return { x = vector.y, y = vector.x }
   end
 
-  return _MAGIC_NUMBER_STACK_LIMIT_PROGRESSION[closest.index]
+  return nil
 end
 
-local function inserter_paste_logic(event, inserter)
-  -- Check #1 - Can I work with this inserter?
-  local pickup_drop_positions = get_real_pickup_dropoff_positions(inserter)
-  if (not pickup_drop_positions) then return end
 
-  -- Check #2 - Can I work with this inserter?
-  local container_at_pickup = get_container(inserter.surface, pickup_drop_positions.pickup)
-  local container_at_dropoff = get_container(inserter.surface, pickup_drop_positions.drop)
+local function connect_neighbor_if_unconnected(entity_connect_from, entity_connect_to)
+  local green_connection = entity_connect_from.get_circuit_network(defines.wire_type.green)
+  local red_connection = entity_connect_from.get_circuit_network(defines.wire_type.red)
 
-  local container_to_connect = container_at_pickup or container_at_dropoff
-  local circuit_condition_comparator = container_at_pickup and ">" or "<"
-
-  -- Make a connection, if one doesn't exist
-  local green_connection = inserter.get_circuit_network(defines.wire_type.green)
-  local red_connection = inserter.get_circuit_network(defines.wire_type.red)
-  if ((not green_connection) and (not red_connection) and container_to_connect)
+  if (not red_connection and not green_connection)
   then
-    inserter.connect_neighbour {
+    -- The only time there WON'T be a circuit connection after this function,
+    -- ... is if there isn't a current connection AND no new connection could be made.
+    if (not entity_connect_to) then return false end
+
+    entity_connect_from.connect_neighbour {
       wire = defines.wire_type.green,
-      target_entity = container_to_connect
+      target_entity = entity_connect_to
     }
   end
 
-  -- Don't try setting stack size if there is no connection
-  if (not (green_connection or red_connection or container_to_connect))
-  then
-    return
+  return true
+end
+
+
+local function set_enable_condition(event, entity, item_name, circuit_condition_comparator)
+  -- Nested utility function for better organization
+  local function get_next_stack_amount(current_stacks)
+    local immediate_lookup = _MAGIC_NUMBER_STACK_LIMIT_PROGRESSION[current_stacks]
+    if (immediate_lookup) then return immediate_lookup end
+
+    local closest = { index = 1, diff = 1000 }
+    for k, v in pairs(_MAGIC_NUMBER_STACK_LIMIT_PROGRESSION) do
+      local difference = math.abs(current_stacks - v)
+      if (difference <= closest.diff)
+      then
+        closest.index = k
+        closest.diff = difference
+      end
+    end
+
+    return _MAGIC_NUMBER_STACK_LIMIT_PROGRESSION[closest.index]
   end
 
-  -- Set the circuit condition
-  -- Determine # of stacks for inserter limit
-  local circuit_settings = inserter.get_or_create_control_behavior()
+  local circuit_settings = entity.get_or_create_control_behavior()
   local circuit_condition = circuit_settings["circuit_condition"].condition
 
   local stacks_to_limit = 1
-  if (circuit_condition.first_signal.name == _item_name)
+  if (circuit_condition.first_signal.name == item_name)
   then
     local current_stacks = math.floor(circuit_condition.constant / _item_stack_size)
     stacks_to_limit = get_next_stack_amount(current_stacks)
@@ -160,14 +130,198 @@ local function inserter_paste_logic(event, inserter)
   local circuit_limit_value = stacks_to_limit * _item_stack_size
   circuit_settings["circuit_condition"] = {
     condition = {
-      first_signal = { type = "item", name = _item_name },
+      first_signal = { type = "item", name = item_name },
       comparator = circuit_condition_comparator,
       constant = circuit_limit_value
     }
   }
 
-  local floating_text = "[item=" .. _item_name .. "] " .. circuit_condition_comparator .. " " .. circuit_limit_value
-  create_flying_text(game.players[event.player_index], floating_text, inserter.position)
+  local floating_text = "[item=" .. item_name .. "] " .. circuit_condition_comparator .. " " .. circuit_limit_value
+  create_flying_text(game.players[event.player_index], floating_text, entity.position)
+end
+
+
+
+-- ===============
+-- Main Logic Here
+-- ===============
+
+
+local function inserter_paste_logic(event, inserter)
+  -- Defined as nested functions to keep things organized
+  local function inserter_get_real_target_positions(inserter_entity)
+    -- Inserter pickup/drop vectors need a translation
+    -- ... based on the entity's rotation.
+    local pickup = standardize_vector(inserter_entity.prototype.inserter_pickup_position)
+    local dropoff = standardize_vector(inserter_entity.prototype.inserter_drop_position)
+
+    local ret_pickup = rotate_vector_from_north(pickup, inserter_entity.direction)
+    local ret_dropoff = rotate_vector_from_north(dropoff, inserter_entity.direction)
+
+    -- These should only be nil if the inserter has diagonal rotation
+    if (not ret_pickup or not ret_dropoff) then return nil end
+
+    local inserter_position = standardize_vector(inserter_entity.position)
+    local ret = {
+      pickup = {
+        inserter_position.x + ret_pickup.x,
+        inserter_position.y + ret_pickup.y
+      },
+      drop = {
+        inserter_position.x + ret_dropoff.x,
+        inserter_position.y + ret_dropoff.y
+      }
+    }
+
+    return ret
+  end
+  local function inserter_get_container(surface, position)
+    local at_pickup = surface.find_entities_filtered {
+      position = position,
+      type = { "container", "logistic-container" }
+    }
+    return (#at_pickup > 0 and at_pickup[1]) or nil
+  end
+
+  -- Check #1 - Can I work with this inserter?
+  local pickup_drop_positions = inserter_get_real_target_positions(inserter)
+  if (not pickup_drop_positions) then return end
+
+  local container_at_pickup = inserter_get_container(inserter.surface, pickup_drop_positions.pickup)
+  local container_at_dropoff = inserter_get_container(inserter.surface, pickup_drop_positions.drop)
+
+  local container_to_connect = container_at_pickup or container_at_dropoff
+  local circuit_condition_comparator = container_at_pickup and ">" or "<"
+
+  if (not connect_neighbor_if_unconnected(inserter, container_to_connect))
+  then
+    return
+  end
+
+  -- Set the circuit condition
+  set_enable_condition(event, inserter, _item_name, circuit_condition_comparator)
+end
+
+
+local function transport_belt_paste_logic(event, belt)
+  -- Defining helper as a nested function to try and keep things organized
+  local function belt_get_container(neighbors)
+    if (#neighbors == 0) then return nil end
+
+    local loader = nil
+    for _, neighbor in ipairs(neighbors) do
+      if (_LOADER_ENTITY_TYPES[neighbor.type])
+      then
+        loader = neighbor
+      end
+    end
+
+    if (not loader) then return nil end
+    return loader.loader_container
+  end
+
+  local belt_neighbors = belt.belt_neighbours
+
+  -- TEST belt with no neighbors, or just input/just output
+  -- TEST belt with underground in/output (should be fine)
+
+  local output_container_entity = belt_get_container(belt_neighbors["outputs"])
+  local input_container_entity = belt_get_container(belt_neighbors["inputs"])
+
+  local container_to_connect = input_container_entity or output_container_entity
+  local circuit_condition_comparator = input_container_entity and ">" or "<"
+
+  if (not connect_neighbor_if_unconnected(belt, container_to_connect))
+  then
+    return
+  end
+
+
+  -- Set the circuit condition
+  -- Determine # of stacks for inserter limit
+  -- https://lua-api.factorio.com/latest/classes/LuaTransportBeltControlBehavior.html
+
+  -- Might not need the below two conditions
+  -- circuit_condition.read_contents = false
+  -- circuit_condition.enable_disable = true
+
+  set_enable_condition(event, belt, _item_name, circuit_condition_comparator)
+end
+
+
+local function assembler_copy_logic(entity)
+  clear_copied_info()
+
+  -- LuaRecipe
+  local recipe = entity.get_recipe()
+  if (not recipe) then return false end
+
+  local item_name = recipe.prototype.products[1].name
+  if (#recipe.prototype.products ~= 1)
+  then
+    return false
+  end
+
+  return set_item_name_and_stack(item_name)
+end
+
+local function container_copy_logic(entity)
+  -- Nested function for better organization
+  local function container_get_inventory_item_name(container)
+    local container_inventory = container.get_inventory(defines.inventory.chest)
+    local inventory_contents = container_inventory.get_contents()
+
+    local item_names = {}
+    for k, _ in pairs(inventory_contents) do
+      table.insert(item_names, k)
+    end
+    if (#item_names == 1)
+    then
+      return item_names[1]
+    else
+      return nil
+    end
+  end
+
+  clear_copied_info()
+
+  local logistic_mode = entity.prototype.logistic_mode
+  if (not logistic_mode)
+  then
+    local item_name = container_get_inventory_item_name(entity)
+    return set_item_name_and_stack(item_name)
+  end
+
+  -- Storage Chests
+  if (logistic_mode == "storage" and entity["storage_filter"])
+  then
+    local item_name = entity["storage_filter"]
+    return set_item_name_and_stack(item_name)
+  end
+
+  -- Buffer / Requester Chests
+  if (logistic_mode == "requester" or logistic_mode == "buffer")
+  then
+    local requests = {}
+    local request_slot_index = entity.request_slot_count
+    while (request_slot_index > 0) do
+      local item_stack = entity.get_request_slot(request_slot_index)
+      if (item_stack)
+      then
+        table.insert(requests, (item_stack.name or item_stack))
+      end
+      request_slot_index = request_slot_index - 1
+    end
+
+    if (#requests == 1)
+    then
+      return set_item_name_and_stack(requests[1])
+    end
+  end
+
+  -- Logistic Chest fallback
+  local item_name = container_get_inventory_item_name(entity)
+  return set_item_name_and_stack(item_name)
 end
 
 
@@ -175,41 +329,45 @@ end
 -- Event Definitions
 -- =================
 
+local _VALID_PMLS_COPY_TARGETS = {
+  ["assembling-machine"] = assembler_copy_logic
+
+
+  -- After playing around with it, I've found that copying
+  -- ... from containers interferes with the expected NORMAL
+  -- ... copy-paste settings capability of containers.
+  -- >> Best to keep this mod focused on transferring settings
+  -- ... between entities you normally CAN'T transfer.
+  -- ["container"] = container_copy_logic,
+  -- ["logistic-container"] = container_copy_logic
+}
+
 
 script.on_event("pmls-copy", function(event)
-  -- LuaEntity
   local entity = game.players[event.player_index].selected
   if (not entity) then
     clear_copied_info()
     return
   end
 
-  if (entity.prototype.type ~= "assembling-machine") then return end
 
-  -- LuaRecipe
-  local recipe = entity.get_recipe()
-  if (not recipe) then
-    clear_copied_info()
-    return
-  end
+  local settings_copy_function = _VALID_PMLS_COPY_TARGETS[entity.prototype.type]
+  if (not settings_copy_function) then return end
 
-  if (#recipe.prototype.products ~= 1)
-  then
-    create_flying_text(game.players[event.player_index], "[virtual-signal=signal-red]", entity.position)
-    clear_copied_info()
-    return
-  end
 
-  -- Need to check item prototypes, as some recipes output fluids.
-  local item_name = recipe.prototype.products[1].name
-  if (game.item_prototypes[item_name])
-  then
-    _item_name = item_name
-    _item_stack_size = game.item_prototypes[_item_name].stack_size
+  local settings_copy_was_successful = settings_copy_function(entity)
+  local text = (
+    settings_copy_was_successful
+    and "[item=" .. _item_name .. "][virtual-signal=signal-green]"
+    or "[virtual-signal=signal-red]"
+  )
 
-    create_flying_text(game.players[event.player_index], "[item=" .. _item_name .. "][virtual-signal=signal-green]",
-      entity.position)
-  end
+  -- Give player feedback
+  create_flying_text(
+    game.players[event.player_index],
+    text,
+    entity.position
+  )
 end)
 
 
@@ -223,6 +381,11 @@ script.on_event("pmls-paste", function(event)
   if (entity.prototype.type == "inserter")
   then
     inserter_paste_logic(event, entity)
+  end
+
+  if (entity.prototype.type == "transport-belt")
+  then
+    transport_belt_paste_logic(event, entity)
   end
 
   -- https://lua-api.factorio.com/latest/classes/LuaEntityPrototype.html#logistic_mode
